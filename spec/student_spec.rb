1cd7a304436bf3f462a7025b1bffb5e3844a8719
Leslie Osorio <lesosorio17@gmail.com>
2019-03-12 22:24:46 +0000

    Automatically backed up by Learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -6,8 +6,8 @@ describe Student do
 
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY, 
-      name TEXT, 
+      id INTEGER PRIMARY KEY,
+      name TEXT,
       grade INTEGER
       )
     SQL
@@ -21,41 +21,41 @@ describe Student do
       id: nil,
       name: "Sam",
       grade: 11
-    } 
+    }
   }
 
   let(:new_student) {Student.new(attributes)}
 
   describe 'inheritance' do
     it 'inherits from InteractiveRecord class' do
-      expect(Student).to be < InteractiveRecord 
+      expect(Student).to be < InteractiveRecord
     end
   end
 
-  describe '.table_name' do 
-    it 'creates a downcased, plural table name based on the Class name' do 
+  describe '.table_name' do
+    it 'creates a downcased, plural table name based on the Class name' do
       expect(Student.table_name).to eq('students')
     end
   end
 
-  describe '.column_names' do 
-    it 'returns an array of SQL column names' do 
+  describe '.column_names' do
+    it 'returns an array of SQL column names' do
       expect(Student.column_names).to eq(["id", "name", "grade"])
     end
   end
 
-  describe 'initialize' do 
-    it 'creates an new instance of a student' do 
+  describe 'initialize' do
+    it 'creates an new instance of a student' do
       expect(Student.new).to be_a Student
     end
 
-    it 'creates a new student with attributes' do 
+    it 'creates a new student with attributes' do
       expect(new_student.name).to eq("Sam")
     end
-  end  
-  
-  describe 'attr_accessor' do 
-    it 'creates attr_accessors for each column name' do 
+  end
+
+  describe 'attr_accessor' do
+    it 'creates attr_accessors for each column name' do
       old_name = new_student.name
       new_name = new_student.name = "Jo"
       old_grade = new_student.grade
@@ -67,28 +67,28 @@ describe Student do
     end
   end
 
-  context 'has instance methods to insert data into db' do 
-    describe '#table_name_for_insert' do 
-      it 'return the table name when called on an instance of Student' do 
+  context 'has instance methods to insert data into db' do
+    describe '#table_name_for_insert' do
+      it 'return the table name when called on an instance of Student' do
         expect(new_student.table_name_for_insert).to eq("students")
       end
     end
 
-    describe '#col_names_for_insert' do 
-      it 'return the column names when called on an instance of Student' do 
+    describe '#col_names_for_insert' do
+      it 'return the column names when called on an instance of Student' do
         expect(new_student.col_names_for_insert).to include("name, grade")
       end
 
-      it 'does not include an id column' do 
+      it 'does not include an id column' do
         expect(new_student.col_names_for_insert).not_to include("id")
       end
     end
 
-    describe '#values_for_insert' do 
-      it 'formats the column names to be used in a SQL statement' do 
+    describe '#values_for_insert' do
+      it 'formats the column names to be used in a SQL statement' do
         expect(new_student.values_for_insert).to eq("'Sam', '11'")
       end
     end
-    
-    describe '#save' do 
+
+    describe '#save' do
   
