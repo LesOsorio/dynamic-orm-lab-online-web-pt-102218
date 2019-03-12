d707b2fee3c67a84bfdb790e56237ac15a9ecc48
Leslie Osorio <lesosorio17@gmail.com>
2019-03-12 22:22:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,19 +8,37 @@ class InteractiveRecord
   end
 
   def self.column_names
+    DB[:conn].results_as_hash = true
+
+    table_columns = DB[:conn].execute("PRAGMA table_info(#{table_name})")
     column_names = []
-    sql = "PRAGMA table_info('#{table_name}')"
-    table_info = DB[:conn].execute(sql)
-    table_info.each do |col|
+
+    table_columns.each do |col|
       column_names << col["name"]
     end
+
     column_names.compact
   end
 
-  def initialize(options = {})
-    options.each do |key,value|
-      self.send(("#{key}="),value)
+  def initialize(objects={})
+    objects.each do |k, v|
+      self.send("#{k}=", v)
     end
+  end
+
+  # ORM Methods
+
+  # SAVE - INSERT INTO students(name, grade) VALUES (x, y)
+
+  def save
+    sql = <<-SQL
+      INSERT INTO #{table_name_for_insert}(#{col_names_for_insert})
+      VALUES (#{values_for_insert})
+      SQL
+
+    DB[:conn].execute(sql)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
   def table_name_for_insert
@@ -28,36 +46,37 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|column_name| column_name == "id"}.join (", ")
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def values_for_insert
-   values_array = []
-   self.class.column_names.each do |column_name|
-     values_array << "'#{send(column_name)}'" unless send(column_name).nil?
-   end
-   values_array.join(", ")
- end
+    values = []
 
- def save
-   sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-   DB[:conn].execute(sql)
-   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-    row = DB[:conn].execute(sql,name)
+    sql = <<-SQL
+      SELECT * FROM #{table_name}
+      WHERE name = ?
+      SQL
+
+    DB[:conn].execute(sql, name);
   end
 
   def self.find_by(attribute)
-    attribute_key = attribute.keys.join()
-    attrubute_value = attribute.values.first
-    sql =<<-SQL
-      SELECT * FROM #{self.table_name}
-      WHERE #{attribute_key} = "#{attrubute_value}"
-      LIMIT 1
-    SQL
-    row = DB[:conn].execute(sql)
+    column_name = attribute.keys[0].to_s
+    value_name = attribute.values[0]
+
+    sql = <<-SQL
+      SELECT * FROM #{table_name}
+      WHERE #{column_name} = ?
+      SQL
+
+    DB[:conn].execute(sql, value_name);
   end
+
 end
