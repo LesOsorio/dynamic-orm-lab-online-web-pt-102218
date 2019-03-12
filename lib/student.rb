2c6f98e4752c83cb53aa6451f458310b4202201e
Leslie Osorio <lesosorio17@gmail.com>
2019-03-12 22:14:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,10 +6,10 @@ class Student < InteractiveRecord
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
-  end 
+  end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'" 
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
 
